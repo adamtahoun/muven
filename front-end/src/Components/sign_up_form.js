0b021c6c6149a3a47f8b8f027abc82e9571878ae
Elias Garcia <elias.garcia.405@gmail.com>
2017-11-28 16:31:36 -0500

    login/signup data event handling

--- front-end/src/Components/sign_up_form.js
+++ front-end/src/Components/sign_up_form.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardActions } from 'material-ui/Card';
 import {Link} from 'react-router-dom';
-
+import axios from 'axios';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -14,7 +14,46 @@ import "../App.css"
     margin: 8,
 };
 class SignupForm extends Component{
+	
+handleClick(event){
+    var apiBaseUrl = "http://localhost:3001";
+    console.log("values",this.state.first_name,this.state.email,this.state.password,this.state.confirm);
+    //To be done:check for empty values before hitting submit
+    var self = this;
+    var payload={
+    "first_name": this.state.first_name,
+    "email":this.state.email,
+    "password":this.state.password,
+    "confirm":this.state.confirm
+    }
+    axios.post(apiBaseUrl+'/signup', payload)
+   .then(function (response) {
+     console.log(response);
+     if(response.data.code == 200){
+       console.log("registration successfull");
+      /*  var loginscreen=[];
+       loginscreen.push(<Login parentContext={this}/>);
+       var loginmessage = "Not Registered yet.Go to registration";
+       self.props.parentContext.setState({loginscreen:loginscreen,
+       loginmessage:loginmessage,
+       buttonLabel:"Register",
+       isLogin:true */
+      //  });
+     }
+   })
+   .catch(function (error) {
+     console.log(error);
+   });
 
+}
+	  constructor(props){
+    super(props);
+    this.state={
+      first_name:'',
+      email:'',
+      password:'',
+	  confirm:''
+	  }}
 	
   render(){
     return(
@@ -34,11 +73,13 @@ class SignupForm extends Component{
 						<TextField
 						hintText="Enter your Username"
 						floatingLabelText="Username"
+						onChange = {(event,newValue) => this.setState({first_name:newValue})}
 						/>
 						<br />
 						<TextField
 						hintText="Enter an Email"
 						floatingLabelText="Email"
+						onChange = {(event,newValue) => this.setState({email:newValue})}
 						
 						/>
 						<br />
@@ -46,18 +87,20 @@ class SignupForm extends Component{
 						hintText="Create a Password"
 						floatingLabelText="Password"
 						type="password"
+						onChange = {(event,newValue) => this.setState({password:newValue})}
 						/>
 						<br />
 						<TextField
 						hintText="Confirm Password"
 						floatingLabelText="Confirm Password"
 						type="password"
+						onChange = {(event,newValue) => this.setState({confirm:newValue})}
 						/>
 					</div>
 					<br/>
 					
 					<CardActions>
-					<RaisedButton label ="Sign Up!" primary = {true} style= {buttonStyle}/>
+					<RaisedButton label ="Sign Up!" primary = {true} style= {buttonStyle} onClick={(event) => this.handleClick(event)}/>
 					<br/>
 					<Link to="./login">
     <RaisedButton label = "Already have an Account?" primary = {true} style= {buttonStyle}/>
