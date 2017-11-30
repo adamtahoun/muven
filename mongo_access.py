29b89b275e5d74a50d20dba2701ab21adbf5ffbb
aandr075 <aandr075@fiu.edu>
2017-11-30 11:24:23 -0500

    added flask backend part
    flask server, not completed yet, but basic idea of what to do

---
+++ mongo_access.py
@@ -0,0 +1,68 @@
+
+from werkzeug import generate_password_hash
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+db = MongoClient()
+
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'Muven'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/Muven'
+
+mongo = PyMongo(app)
+
+app.secret_key = "muven4-key"
+
+@app.route('/signup', methods=['POST'])
+def sign up():
+    if request.method == "POST":
+        if 'artist' in session:
+            user = mongo.db.artist
+        if 'venue' in session:
+            user = mongo.db.venue
+        
+        //gather data
+        user = request.form['first_name']
+        email = request.form['email']
+        if (request.form['password'] == request.form['confirm']):
+            pwdhash = generate_password_hash(request.form['password'])
+            
+        user_record = {"username": user,
+                       "email": email,
+                       "password" : pwdhash   }
+        
+        if user.find_one({"email": email}) != None:
+            return 2 //email already registered error
+        elif user.find_one({"username": user}) != None:
+            return 1 //username already in use error
+        else
+            user.insert_one(user_record)
+            return 0 //successfully registered
+    return 3 //nothing posted
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == "POST":
+        artist = mongo.db.artist
+        venue = mondo.db.venue
+        email = request.form['email']
+        pwdhash  = generate_password_hash(request.form['password'])
+        
+        if venue.find_one({"email": email, "password": pwdhash}) != None:
+            return 0 //logged in as a venue
+        elif artist.find_one({"email": email, "password": pwdhash}) != None:
+            return 1 //logged in as an artist
+        else
+            return 2 //no matches
+        
+    return 3 //nothing posted
+    
+
+    
+    
+if __name__ == "__main__":
+    app.run(debug=True)
