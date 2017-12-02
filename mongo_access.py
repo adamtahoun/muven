93ee495ae5ea36db83f01865b84c2b8e35e8e676
aandr075 <aandr075@fiu.edu>
2017-12-02 05:58:07 -0500

    improved code
    flask should work with react, but could not get react working to test it.

--- mongo_access.py
+++ mongo_access.py
@@ -21,68 +21,68 @@ mongo = PyMongo(app)
 
 app.secret_key = "muven4-key"
 
-@app.route('/api/signup', methods=['POST'])
-def sign up():
+@app.route('/signup', methods=['POST'])
+def sign_up():
+    data = request.get_json(silent=true)
     if request.method == "POST":
-        if 'artist' in session:
-            user = mongo.db.artist
-        if 'venue' in session:
-            user = mongo.db.venue
+        #if 'artist' in data:
+        #    user = mongo.db.artist
+        #if 'venue' in data:
+        #    user = mongo.db.venue
+        user = mongo.db.user #user collection until artist/venue functionality added
         
-        //gather data
-        user = request.form['first_name']
-        email = request.form['email']
-        if (request.form['password'] == request.form['confirm']):
-            pwdhash = generate_password_hash(request.form['password'])
-            
+        #gather data
+        user = data.get('first_name')
+        email = data.get('email')
+        #verify that passwords match and encrypt the password
+        if (data.get('password') == data.get('confirm')): 
+            pwdhash = generate_password_hash(data.get('password'))
+        
+        #user record to be stored
         user_record = {"username": user,
                        "email": email,
                        "password" : pwdhash   }
         
-        if user.find_one({"email": email}) != None:
-            return 2 //email already registered error
-        elif user.find_one({"username": user}) != None:
-            return 1 //username already in use error
-        else
+        #if the email is already in use
+        if user.find_one({"email": email}):
+            return 409 
+        #if the username is already in use
+        elif user.find_one({"username": user}) :
+            return 406 #username already in use error
+        else:
             user.insert_one(user_record)
-            return 0 //successfully registered
-    return 3 //nothing posted
+            return 200 #successfully registered
+        
+    return 400 #nothing posted
 
-@app.route('/api/login', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
     if request.method == "POST":
-        artist = mongo.db.artist
-        venue = mondo.db.venue
-        email = request.form['email']
-        pwdhash  = generate_password_hash(request.form['password'])
+        #artist = mongo.db.artist
+        #venue = mondo.db.venue
+        user = mongo.db.user #user collection until artist/venue functionality added
         
-        if venue.find_one({"email": email, "password": pwdhash}) != None:
-            return 0 //logged in as a venue
-        elif artist.find_one({"email": email, "password": pwdhash}) != None:
-            return 1 //logged in as an artist
-        else
-            return 2 //no matches
+        #get data from axios?
+        email = data.get('email')
+        pwdhash  = generate_password_hash(data.get('password'))
         
-    return 3 //nothing posted
+        #This would check both collections for the username/password combo
+        #if venue.find_one({"email": email, "password": pwdhash}) != None:
+        #    return 200 #logged in as a venue
+        #elif artist.find_one({"email": email, "password": pwdhash}) != None:
+        #    return 200 #logged in as an artist
+        
+        #if username and password find no match
+        if(!user.find_one({"email": email, "password": pwdhash}))
+            return 204
+        elif(!user.find_one({"email": email}))
+            return 0 #no matches
+        
+        return 400 #nothing posted -must enter username and password-
     
-@app.route('/api/search', methods=['POST'])
-def login():
-    if request.method == "POST":
-        artist = mongo.db.artist
-        venue = mondo.db.venue
-        email = request.form['email']
-        pwdhash  = generate_password_hash(request.form['password'])
-        
-        if venue.find_one({"email": email, "password": pwdhash}) != None:
-            return 0 //logged in as a venue
-        elif artist.find_one({"email": email, "password": pwdhash}) != None:
-            return 1 //logged in as an artist
-        else
-            return 2 //no matches
-        
-    return 3 //nothing posted
+    return 404#nothing posted
+    
 
-    
     
 if __name__ == "__main__":
     app.run(debug=True)
