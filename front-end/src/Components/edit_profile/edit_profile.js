a6e18eac2dc60f68b4a6632a29e912ab5283b483
Adam Tahoun <ataho001@fiu.edu>
2017-12-05 14:56:06 -0500

    ls

--- front-end/src/Components/edit_profile/edit_profile.js
+++ front-end/src/Components/edit_profile/edit_profile.js
@@ -22,6 +22,11 @@ const style = {
   marginLeft: 20,
 };
 
+const buttonStyle = {
+  margin: 8,
+  position:
+};3
+
 
 class EditProfile extends Component{
 
@@ -29,20 +34,33 @@ class EditProfile extends Component{
    super(props);
    this.state = {
      value: 'artist',
-     artist: {
-       name: '',
-       address: '',
-       city: '',
-       state: '',
-       genre:'',
-     }
+     artist_name: '',
+     artist_address: '',
+     artist_city: '',
+     artist_state: '',
+     artist_genre:'',
+     artist_about:'',
+     venue_name:'',
+     venue_address: '',
+     venue_city: '',
+     venue_state: '',
+     venue_max_bands:'',
+     venue_capacity: '',
+     venue_genre:'',
+     venue_about:''
    };
- }
+   console.log(this.state);
+ };
 
  handleChange = (value) => {
    this.setState({
      value: value,
    });
+ };
+
+ handleClick(event){
+   var apiBaseUrl = "http://localhost:5000";
+   console.log(this.state);
  };
 
   render(){
@@ -58,39 +76,105 @@ class EditProfile extends Component{
             <Tab label="Artist" value="artist">
               <div>
               <Paper zDepth={2}>
-                <TextField hintText="Artist name" style={style} underlineShow={false} />
+                <TextField
+                  hintText="Artist name"
+                  style={style}
+                  underlineShow={false}
+                  onChange = {(event,newValue) => this.setState({artist_name:newValue})}
+                />
                 <Divider />
-                <TextField hintText="Address" style={style} underlineShow={false} />
+                <TextField
+                  hintText="Address"
+                  style={style}
+                  underlineShow={false}
+                  onChange = {(event,newValue) => this.setState({artist_address:newValue})}
+                />
                 <Divider />
-                <TextField hintText="City" style={style} underlineShow={false} />
+                <TextField
+                  hintText="City"
+                  style={style}
+                  underlineShow={false}
+                  onChange = {(event,newValue) => this.setState({artist_city:newValue})}
+                />
                 <Divider />
-                <TextField hintText="State" style={style} underlineShow={false} />
+                <TextField
+                  hintText="State"
+                  style={style}
+                  underlineShow={false}
+                  onChange = {(event,newValue) => this.setState({artist_state:newValue})}
+                />
                 <Divider />
-                <TextField hintText="Genre" style={style} underlineShow={false} />
+                <TextField
+                  hintText="Genre"
+                  style={style}
+                  underlineShow={false}
+                  onChange = {(event,newValue) => this.setState({artist_genre:newValue})}
+                />
                 <Divider />
-                <TextField hintText="About" style={style} underlineShow={false} />
+                <TextField
+                  hintText="About"
+                  style={style}
+                  underlineShow={false}
+                  onChange = {(event,newValue) => this.setState({artist_about:newValue})}
+                />
             </Paper>
               </div>
             </Tab>
             <Tab label="Venue" value="venue">
             <Paper zDepth={2}>
-              <TextField hintText="Venue name" style={style} underlineShow={false} />
+              <TextField
+                hintText="Venue name"
+                style={style}
+                underlineShow={false}
+                onChange = {(event,newValue) => this.setState({venue_name:newValue})}
+              />
               <Divider />
-              <TextField hintText="Address" style={style} underlineShow={false} />
+              <TextField
+                hintText="Address"
+                style={style}
+                underlineShow={false}
+                onChange = {(event,newValue) => this.setState({venue_address:newValue})}
+              />
               <Divider />
-              <TextField hintText="City" style={style} underlineShow={false} />
+              <TextField
+                hintText="City"
+                style={style}
+                underlineShow={false}
+                onChange = {(event,newValue) => this.setState({venue_city:newValue})}
+              />
               <Divider />
-              <TextField hintText="Capacity" style={style} underlineShow={false} />
+              <TextField
+                hintText="Capacity"
+                style={style}
+                underlineShow={false}
+                onChange = {(event,newValue) => this.setState({venue_capacity:newValue})}
+              />
               <Divider />
-              <TextField hintText="Max Bands" style={style} underlineShow={false} />
+              <TextField
+                hintText="Max Bands"
+                style={style}
+                underlineShow={false}
+                onChange = {(event,newValue) => this.setState({venue_max_bands:newValue})}
+              />
               <Divider />
-              <TextField hintText="Genre" style={style} underlineShow={false} />
+              <TextField
+                hintText="Genre"
+                style={style}
+                underlineShow={false}
+                onChange = {(event,newValue) => this.setState({venue_genre:newValue})}
+              />
               <Divider />
-              <TextField hintText="About" style={style} underlineShow={false} />
+              <TextField
+                hintText="About"
+                style={style}
+                underlineShow={false}
+                onChange = {(event,newValue) => this.setState({venue_about:newValue})}
+              />
               <Divider />
           </Paper>
             </Tab>
           </Tabs>
+            <RaisedButton label ="Submit" primary = {true} style= {buttonStyle} onClick={(event) => this.handleClick(event)}/>
          </Card>
         </MuiThemeProvider>
       </div>
